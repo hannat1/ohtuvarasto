570e29d15fbe36c36de48cdb7eefa123e0c0fb6d
Hanna Turunen <hannamatilda.turunen@gmail.com>
2024-10-30 12:40:53 +0200

    Varasto luokan testit

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -4,8 +4,15 @@ from varasto import Varasto
 
 class TestVarasto(unittest.TestCase):
     def setUp(self):
+        self.varasto = Varasto(0)
+        self.varasto = Varasto(10, -1)        
+        self.varasto = Varasto(1, 2)
+        self.varasto = Varasto(2, 1)
         self.varasto = Varasto(10)
 
+    def test_str(self):
+        self.assertEqual(str(self.varasto), "saldo = 0, vielä tilaa 10")
+        
     def test_konstruktori_luo_tyhjan_varaston(self):
         # https://docs.python.org/3/library/unittest.html#unittest.TestCase.assertAlmostEqual
         self.assertAlmostEqual(self.varasto.saldo, 0)
@@ -15,8 +22,14 @@ class TestVarasto(unittest.TestCase):
 
     def test_lisays_lisaa_saldoa(self):
         self.varasto.lisaa_varastoon(8)
-
         self.assertAlmostEqual(self.varasto.saldo, 8)
+
+        self.varasto.lisaa_varastoon(-1)
+        self.assertAlmostEqual(self.varasto.saldo, 8)
+
+        self.varasto.lisaa_varastoon(11)
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+
 
     def test_lisays_lisaa_pienentaa_vapaata_tilaa(self):
         self.varasto.lisaa_varastoon(8)
@@ -26,10 +39,15 @@ class TestVarasto(unittest.TestCase):
 
     def test_ottaminen_palauttaa_oikean_maaran(self):
         self.varasto.lisaa_varastoon(8)
-
         saatu_maara = self.varasto.ota_varastosta(2)
-
         self.assertAlmostEqual(saatu_maara, 2)
+        
+        saatu_maara = self.varasto.ota_varastosta(-1)
+        self.assertAlmostEqual(saatu_maara, 0)
+
+        self.varasto.lisaa_varastoon(4)
+        saatu_maara = self.varasto.ota_varastosta(15)
+        self.assertAlmostEqual(saatu_maara, 10)
 
     def test_ottaminen_lisaa_tilaa(self):
         self.varasto.lisaa_varastoon(8)
